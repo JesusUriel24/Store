13ad7373fafc163a86b6952a34c9b846534eb9a0
Jesus Uriel <solonosabes@gmail.com>
2021-12-16 12:21:24 -0600

    Listener y ViewHolder

--- app/src/main/java/com/example/stores1/StoreEntity.kt
+++ app/src/main/java/com/example/stores1/StoreEntity.kt
@@ -3,5 +3,5 @@ package com.example.stores1
 data class Store(val id: Long =0,
                  var name:String,
                  var phone: String="",
-                 var website: String,
+                 var website: String="",
                  var isFavorite: Boolean= false)
